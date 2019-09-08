76878f8df63fb91a9efa4f95d11ed3cdb374b942
KARLSZP <song8748@hotmail.com>
2019-09-08 11:48:22 +0800

    Updated

--- Experiment/Exp2/E02_20190904_15puzzle/sol_exp2_cpp.cpp
+++ Experiment/Exp2/E02_20190904_15puzzle/sol_exp2_cpp.cpp
@@ -2,8 +2,20 @@
 #include <cstdlib>
 using namespace std;
 
+
+int ** load(const int size) {
+    int Map[size][size] = {
+        {7, 5, 2, 4}, 
+        {9, 0, 1, 8},
+        {6, 10, 3, 12},
+        {13, 14, 11, 15}
+    }
+    return Map;
+}
+
+
 int main() {
-    cout<<"test vscode"<<endl;
-    system("pause");
+    const int size = 4;
+    int (*ptr)[size] = load(size);
     return 0;
 }
